ccca09a929dc9a6800e3d7206e057dc4d465d40d
Leah Deshar <leah.desar@gmail.com>
2024-07-24 16:11:21 +0545

    Arith calculate: functionality

--- app/src/main/java/com/example/assignment/assignment2/CalculatorActivity.java
+++ app/src/main/java/com/example/assignment/assignment2/CalculatorActivity.java
@@ -1,6 +1,9 @@
 package com.example.assignment.assignment2;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +15,9 @@ import com.example.assignment.R;
 
 public class CalculatorActivity extends AppCompatActivity {
 
+    private EditText firstEt, secondEt;
+private TextView resultTv;
+private Button addBtn, subBtn, mulBtn, divBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,5 +28,46 @@ public class CalculatorActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        firstEt = findViewById(R.id.first_number);
+        secondEt = findViewById(R.id.second_number);
+        resultTv = findViewById(R.id.result);
+        addBtn = findViewById(R.id.add);
+        subBtn = findViewById(R.id.subtract);
+        mulBtn = findViewById(R.id.multiply);
+        divBtn = findViewById(R.id.divide);
+
+        addBtn.setOnClickListener(v -> {
+            double first = Double.parseDouble(firstEt.getText().toString());
+            double second = Double.parseDouble(secondEt.getText().toString());
+            double result = first + second;
+            resultTv.setText("Result: " + result);
+        });
+
+        subBtn.setOnClickListener(v -> {
+            double first = Double.parseDouble(firstEt.getText().toString());
+            double second = Double.parseDouble(secondEt.getText().toString());
+            double result = first - second;
+            resultTv.setText("Result: " + result);
+        });
+
+        mulBtn.setOnClickListener(v -> {
+            double first = Double.parseDouble(firstEt.getText().toString());
+            double second = Double.parseDouble(secondEt.getText().toString());
+            double result = first * second;
+            resultTv.setText("Result: " + result);
+        });
+
+        divBtn.setOnClickListener(v -> {
+            double first = Double.parseDouble(firstEt.getText().toString());
+            double second = Double.parseDouble(secondEt.getText().toString());
+            double result = first / second;
+            resultTv.setText("Result: " + result);
+        });
+
+
+
+
     }
 }
