713df7c2779db1b45aadd27ad24173c7728da883
Leah Deshar <leah.desar@gmail.com>
2024-07-24 16:25:22 +0545

    Assignment completed

--- app/src/main/java/com/example/assignment/assignment2/RectCalculateActivity.java
+++ app/src/main/java/com/example/assignment/assignment2/RectCalculateActivity.java
@@ -1,6 +1,9 @@
 package com.example.assignment.assignment2;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +14,9 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.assignment.R;
 
 public class RectCalculateActivity extends AppCompatActivity {
+    private EditText length,breadth;
+    private TextView result;
+    private Button calculate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +27,19 @@ public class RectCalculateActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        length = findViewById(R.id.lengthEt);
+        breadth = findViewById(R.id.breadthEt);
+        result = findViewById(R.id.result);
+        calculate = findViewById(R.id.calculate);
+
+        calculate.setOnClickListener(v -> {
+            double l = Double.parseDouble(length.getText().toString());
+            double b = Double.parseDouble(breadth.getText().toString());
+            double area = l * b;
+            double perimeter = 2 * (l + b);
+            result.setText("Area of Rectangle: " + area + "\nPerimeter of Rectangle: " + perimeter);
         });
     }
 }
--- app/src/main/java/com/example/assignment/assignment2/SimpleInterestActivity.java
+++ app/src/main/java/com/example/assignment/assignment2/SimpleInterestActivity.java
@@ -1,6 +1,9 @@
 package com.example.assignment.assignment2;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +14,9 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.assignment.R;
 
 public class SimpleInterestActivity extends AppCompatActivity {
+    private EditText principal, rate, time;
+    private TextView result;
+    private Button calculate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +28,21 @@ public class SimpleInterestActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        principal = findViewById(R.id.principal);
+        rate = findViewById(R.id.rate);
+        time = findViewById(R.id.time);
+        result = findViewById(R.id.result);
+        calculate = findViewById(R.id.calculate);
+
+        calculate.setOnClickListener(v -> {
+            double p = Double.parseDouble(principal.getText().toString());
+            double r = Double.parseDouble(rate.getText().toString());
+            double t = Double.parseDouble(time.getText().toString());
+            double si = (p * r * t) / 100;
+            result.setText("Simple Interest: " + si);
+        });
+
+
     }
 }
