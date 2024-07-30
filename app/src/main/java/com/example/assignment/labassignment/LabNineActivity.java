9aa87aef8f2864832c4f77d3333c4a18f629106d
Leah Deshar <leah.desar@gmail.com>
2024-07-30 07:47:01 +0545

    lab assignment

--- app/src/main/java/com/example/assignment/labassignment/LabNineActivity.java
+++ app/src/main/java/com/example/assignment/labassignment/LabNineActivity.java
@@ -1,6 +1,12 @@
 package com.example.assignment.labassignment;
 
 import android.os.Bundle;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +17,7 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.assignment.R;
 
 public class LabNineActivity extends AppCompatActivity {
+    private Button showPopupBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +29,34 @@ public class LabNineActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        showPopupBtn = findViewById(R.id.show_popup_btn);
+        showPopupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPopupMenu(v);
+            }
+        });
+    }
+    private void showPopupMenu(View view) {
+        PopupMenu popup = new PopupMenu(this, view);
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.context_menu, popup.getMenu());
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if(item.getItemId()== R.id.action_edit) {
+                    Toast.makeText(LabNineActivity.this, "Edit selected", Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+                else if(item.getItemId()== R.id.action_delete) {
+                    Toast.makeText(LabNineActivity.this, "Delete selected", Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+                    else{
+                        return false;
+                }
+            }
+        });
+        popup.show();
     }
 }
