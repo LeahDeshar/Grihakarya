d9baf068fb271f1d5a35e8d3c91a84663ed0f976
Leah Deshar <leah.desar@gmail.com>
2024-07-30 07:40:55 +0545

    lab assignment

--- app/src/main/java/com/example/assignment/labassignment/LabEightActivity.java
+++ app/src/main/java/com/example/assignment/labassignment/LabEightActivity.java
@@ -1,6 +1,14 @@
 package com.example.assignment.labassignment;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +19,8 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.assignment.R;
 
 public class LabEightActivity extends AppCompatActivity {
+    private ListView listView;
+    private String[] items = {"Item 1", "Item 2", "Item 3", "Item 4", "Item 5"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +32,34 @@ public class LabEightActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        listView = findViewById(R.id.list_view);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        listView.setAdapter(adapter);
+
+        // Register the ListView for the context menu
+        registerForContextMenu(listView);
     }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.context_menu, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        if(item.getItemId()== R.id.action_edit) {
+            Toast.makeText(this, "Edit " + items[info.position], Toast.LENGTH_SHORT).show();
+            return true;
+        }
+            else if(item.getItemId()==R.id.action_delete) {
+            Toast.makeText(this, "Delete " + items[info.position], Toast.LENGTH_SHORT).show();
+            return true;
+        }
+           else {
+            return super.onContextItemSelected(item);
+        }
+        }
 }
