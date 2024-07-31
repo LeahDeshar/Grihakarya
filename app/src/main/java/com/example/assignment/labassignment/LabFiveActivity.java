5efbc28441ae1df4c8d4f479a9c6b000b35625de
Leah Deshar <leah.desar@gmail.com>
2024-07-31 15:46:04 +0545

    lab assignment

--- app/src/main/java/com/example/assignment/labassignment/LabFiveActivity.java
+++ app/src/main/java/com/example/assignment/labassignment/LabFiveActivity.java
@@ -1,6 +1,12 @@
 package com.example.assignment.labassignment;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +17,8 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.assignment.R;
 
 public class LabFiveActivity extends AppCompatActivity {
+    private EditText phoneEditText;
+    private Button callButton, saveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +29,34 @@ public class LabFiveActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        phoneEditText = findViewById(R.id.phoneEditText);
+        callButton = findViewById(R.id.callButton);
+        saveButton = findViewById(R.id.saveButton);
+
+        callButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phoneNumber = phoneEditText.getText().toString();
+                if (!phoneNumber.isEmpty()) {
+                    Intent intent = new Intent(Intent.ACTION_DIAL);
+                    intent.setData(Uri.parse("tel:" + phoneNumber));
+                    startActivity(intent);
+                }
+            }
+        });
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phoneNumber = phoneEditText.getText().toString();
+                if (!phoneNumber.isEmpty()) {
+                    Intent intent = new Intent(Intent.ACTION_INSERT);
+                    intent.setType(ContactsContract.RawContacts.CONTENT_TYPE);
+                    intent.putExtra(ContactsContract.Intents.Insert.PHONE, phoneNumber);
+                    startActivity(intent);
+                }
+            }
         });
     }
 }
